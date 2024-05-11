cc2441f4fba6381f36a34bf60438d8bd96a6e68f
imran61999 <smemrannit@gmail.com>
2024-05-11 17:25:43 +0600

    all data displayed with card

--- src/Pages/AllBook/AllBook.jsx
+++ src/Pages/AllBook/AllBook.jsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
+import BookCard from "../BookCard/BookCard";
 
 const AllBook = () => {
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
 
     useEffect(()=>{
         fetch('http://localhost:5000/books')
         .then(res => res.json())
         .then(data=>{
-            console.log(data)
+            // console.log(data)
+            setBooks(data)
         })
     },[])
     return (
         <div>
-            
+           <h2>All Book: {books?.length}</h2>
+           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            {
+                books.map(book =><BookCard key={book._id} book={book}></BookCard>)
+            }
+           </div>
         </div>
     );
 };
---
+++ src/Pages/BookCard/BookCard.jsx
@@ -0,0 +1,22 @@
+
+const BookCard = ({book}) => {
+    const {image, name, author_name} = book;
+    return (
+        <div className="card bg-base-100 shadow-xl">
+  <figure><img src={image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+     {name}
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Fashion</div> 
+      <div className="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default BookCard;
